4becb9d9e5113f4c2d9a6536c8ba869171f3c6c3
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-12-02 20:31:05 +0530

    Set up React application with Vite and Tailwind CSS, including initial components and configuration

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const rootElement = document.getElementById('root');
+  return (
+    <>
+      <div className='text-center text-4xl'>hello world</div>
+    </>
+  );
+}
+
+export default App;
+
