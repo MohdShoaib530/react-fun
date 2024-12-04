6d944938598154d6e669a6e159864eb520d8a035
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-12-04 19:05:10 +0530

    Implement background color changer with button in App component

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const rootElement = document.getElementById('root');
+  const [color, setColor] = useState('#fff');
+  const bgChager = () => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = '#' + randomColor;
+    setColor('#' + randomColor);
+  };
   return (
-    <>
-      <div className='text-center text-4xl'>hello world</div>
-    </>
+    <div className='w-full h-screen'>
+      <h1>Background Changer</h1>
+      <p>Click on the button to change the background color.</p>
+      <button className='btn cursor-pointer' onClick={bgChager}>
+        Change Background
+      </button>
+    </div>
   );
 }
 
